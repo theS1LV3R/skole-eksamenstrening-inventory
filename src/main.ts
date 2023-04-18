8c2d0d17548282176a218940b80976aef124f294
monke-boi-hackerman <91062755+monke-hackerman@users.noreply.github.com>
2023-04-18 13:05:47 +0200

    i dont even know
    Co-authored-by: emmae05 <emmae05@users.noreply.github.com>
    Co-authored-by: NoaED05 <NoaED05@users.noreply.github.com>
    Co-authored-by: Zoe <theS1LV3R@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -6,28 +6,45 @@ import bettersqlite from "better-sqlite3";
 import api from "./api";
 import views from "./views";
 import dotenv from "dotenv";
+import { engine } from "express-handlebars";
+import { PrismaClient } from "@prisma/client";
 
-const db = bettersqlite("database.sqlite3");
+
 dotenv.config();
-
+const prisma = new PrismaClient();
 const app = express();
-//start a session
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET ?? "secret",
-    resave: false,
-    saveUninitialized: false,
+
+async function main() {
+  await prisma.$connect();
+
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET ?? "secret",
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+
+  app.engine("handlebars", engine());
+  app.set("view engine", "handlebars");
+  app.set("views", path.resolve(__dirname, "views"));
+
+  app.use("/api", api(prisma));
+  app.use("/", views(prisma));
+
+  const PORT = process.env.PORT ?? 8080;
+
+  app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
   })
-);
-//set up a views engine for our hbs files
-app.set("views", path.resolve(__dirname, "views"));
-app.set("view engine", "hbs");
-
-//send the user to the right place
-app.use("/api", api);
-app.use("/", views);
-
-//start the app at port 8080
-app.listen(process.env.PORT, () => {
-  console.log("Listening on 8080");
-});
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
--- src/views.ts
+++ src/views.ts
@@ -1,4 +1,55 @@
+
+import { PrismaClient, ItemType } from "@prisma/client";
 import { Router } from "express";
 
-const router = Router();
-export default router;
+export default function views(prisma: PrismaClient): Router {
+  const router = Router();
+
+  router.get("/", async (req, res) => {
+    const types = await prisma.itemType.findMany();
+    const itemCounts: Record<ItemType['name'], number> = {}
+
+    types.forEach(async (type) => {
+      const items = await prisma.item.findMany({ where: { type } })
+      itemCounts[type.name] = items.length
+    })
+
+    const pageData: (typeof types[number] & { count: number })[] = []
+    types.forEach(type => {
+      pageData.push({ ...type, count: itemCounts[type.name] })
+    })
+
+    return res.render("FrontPage", { items: pageData })
+  })
+
+
+
+  router.post("/login", async (req: ReqWithBody<LoginRequest>, res) => {
+    const user = await prisma.user.findFirst({
+      where: { name: req.body.username },
+    });
+
+    if (!user) {
+      return res
+        .status(400)
+        .redirect("/login?status=invalid_username_password");
+    }
+
+    if (!(await compare(req.body.password, user.password_hash))) {
+      return res
+        .status(400)
+        .redirect("/login?status=invalid_username_password");
+    }
+
+    req.session.user_id = user.id;
+
+    if (req.params.next) {
+      return res.redirect(decodeURI(req.params.next));
+    }
+
+    return res.redirect("/");
+  });
+
+  return router;
+
+}
